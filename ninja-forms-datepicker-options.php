ab6a4f22ed539c6217d42845b36af8f2e0ae96fe
wpnzach <zach@wpninjas.com>
2015-02-19 09:51:15 -0500

    Update ninja-forms-datepicker-options.php

--- ninja-forms-datepicker-options.php
+++ ninja-forms-datepicker-options.php
@@ -2,13 +2,14 @@
 /*
 Plugin Name: Ninja Forms Add Datepicker Options
 Plugin URI: http://wpninjas.net
-Description: This plugin allows you to add File Upload links automatically to post creations
+Description: This plugin allows you to add jQuery UI options to the datepicker using Ninja Forms
 Version: 1.0
 Author: WPN Zach
 Author URI: http://wpninjas.net
 */
 
 function nf_datepicker_modify_script( $args ){
+	//for a list of arguments which can be used here, see the options here http://api.jqueryui.com/datepicker/
 	$args['minDate'] = "0";
 	$args['changeMonth'] = 1;
 	$args['changeYear'] = 1;
