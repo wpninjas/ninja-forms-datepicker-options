f675d4b3591f9ab3294a541b218d08d2b9a88d8b
Kyle B. Johnson <me@kylebjohnson.me>
2015-02-19 09:42:11 -0500

    remove commented-out example

--- ninja-forms-datepicker-options.php
+++ ninja-forms-datepicker-options.php
@@ -8,21 +8,6 @@ Author: WPN Zach
 Author URI: http://wpninjas.net
 */
 
-// Plugin Folder URL
-//
-// define( 'CUSTOM_NF_DATE_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
-//
-// function nf_datepicker_options( $form_id ) {
-// 	if ($form_id == 9) {
-// 		wp_enqueue_script( 'nf-custom-datepicker-dates' , CUSTOM_NF_DATE_PLUGIN_URL . 'js/cal.js' , array( 'ninja-forms-display' ) );
-// 	}
-// }
-//
-// add_action( 'ninja_forms_display_js' , 'nf_datepicker_options' );
-
-//clean_url('http://www.google.com');
-
-
 function nf_datepicker_modify_script( $args ){
 	$args['minDate'] = "0";
 	$args['changeMonth'] = 1;
